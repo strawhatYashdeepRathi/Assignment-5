0a622db18b24fc9c3939404f5ed1f4bcef1ac419
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2021-11-28 21:48:06 +0530

    can_drive

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,14 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence === false){
+        return("You cannot drive")
+    }
+    else if((isTired === false && isSober === false) || (isTired === true && isSober === false)){
+        return("You shouldn't drive")
+    }
+    else if((isTired === true&& isSober === true) || (isTired === true && isSober === false) || (isTired === false && isSober === true)){
+        return("You can drive")
+    }
 }
 
 module.exports = CanDrive;
