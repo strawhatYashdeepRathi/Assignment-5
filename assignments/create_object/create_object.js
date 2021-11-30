7a1aad05ae67feab6caa226e0b514861eab87ea9
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2021-12-01 00:49:39 +0530

    createObj

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    let dictarr = {};
+    for(var i =0; i<arr.len; i+=2){
+        dictarr[arr[i]] = arr[i+1];
+    }
+    return dictarr
 }
 
 module.exports = CreateObject;
